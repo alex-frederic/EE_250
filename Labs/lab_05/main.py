310341cf30834ca95c4e6369da646eef149cc366
alex-frederic <arfreder@usc.edu>
2025-09-30 19:47:16 -0700

    Finished main.py code

--- Labs/lab_05/main.py
+++ Labs/lab_05/main.py
@@ -53,17 +53,20 @@ def main():
 
     data = [] # list of data points
     for location in locations:
-        print(f"Go to the {location} and press enter to start sampling")
+        print(f"\nGo to the {location} and press enter to start sampling", end="")
         input() # wait for the user to press enter
+        print("Collecting samples...", end="\n")
         signal_strengths = [] # list of signal strengths at this location
 
         # TODO: collect 10 samples of the signal strength at this location, waiting 1 second between each sample
         # HINT: use the get_wifi_signal_strength function
-        
+        for i in range(0, samples_per_location):
+            signal_strengths.append( get_wifi_signal_strength() )
+            time.sleep( time_between_samples )
         
         # TODO: calculate the mean and standard deviation of the signal strengths you collected at this location
-        signal_strength_mean = None
-        signal_strength_std = None
+        signal_strength_mean = np.mean( signal_strengths )
+        signal_strength_std = np.std(signal_strengths)
 
         # Question 6: What is the standard deviation? Why is it useful to calculate it?
         data.append((location, signal_strength_mean, signal_strength_std))
@@ -80,8 +83,12 @@ def main():
     # HINT: https://plotly.com/python/bar-charts/
     # NOTE: use the error_y parameter of px.bar to plot the error bars (1 standard deviation)
     #   documentation: https://plotly.com/python-api-reference/generated/plotly.express.bar.html
+
     fig = px.bar(
-        
+        df,
+        x="location",
+        y="signal_strength_mean",
+        error_y=df.get("signal_strength_std")
     )
     # Question 8: Why is it important to plot the error bars? What do they tell us?
 
