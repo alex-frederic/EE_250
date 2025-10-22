487832c109d41babf1720c3e3847978cce701010
alex-frederic <arfreder@usc.edu>
2025-10-21 19:11:37 -0700

    Updated starter code with our progress so far

--- Labs/lab_07/test_suite.py
+++ Labs/lab_07/test_suite.py
@@ -17,9 +17,10 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 # appropriate threshold levels and set them 
 # accordingly. Then, use them to determine
 # when it is light or dark, quiet or loud.
-lux_treshold=0  # change this value
-sound_treshold=0 # change this value
+lux_threshold=0  # change this value
+sound_threshold=0 # change this value
 
+LED = 11
 
 while True: 
   time.sleep(0.5) 
@@ -27,6 +28,9 @@ while True:
   #Following commands control the state of the output
   #GPIO.output(pin, GPIO.HIGH)
   #GPIO.output(pin, GPIO.LOW)
+  GPIO.output(LED, GPIO.LOW)
+  time.sleep(0.5)
+  GPIO.output(LED, GPIO.HIGH)
 
   # get reading from adc 
   # mcp.read_adc(adc_channel)
