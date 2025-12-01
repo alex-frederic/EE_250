0374c176295f3eaf5b02036d25419c80fd1426d7
Peyton Crawford <pcrawfor@usc.edu>
2025-12-01 14:16:22 -0800

    Trying out new publish code

--- Final Project/mqtt_send.py
+++ Final Project/mqtt_send.py
@@ -1,24 +1,8 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import time
-import socket
 # from yolo import runInference
 
-def on_connect(client, userdata, flags, rc):
-	print("Connected to test.mosqsuitto.org")
-	print("Result Code: " + str(rc))
-
-if __name__ == '__main__':
-	#create a client object
-	client = mqtt.Client()
-	
-	#attach the on_connect() callback function defined above to the mqtt client
-	client.on_connect = on_connect
-
-	client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
-
-	client.loop_start()
-	time.sleep(1)
-	count = 0
+if __name == '__main__':
 	while True:
 		byteArr = 0
 		with open("./rpicamexample.jpg",'rb') as file:
@@ -28,9 +12,41 @@ if __name__ == '__main__':
 	
 
 		print("Publishing...")
-		client.publish("piwatch/send_img", byteArr)
+		publish.single("piwatch/send_img", byteArr, hostname="test.mosquitto.org")
 
 		time.sleep(2)
 		count += 1
 		if(count == 5):
 			break
+
+# def on_connect(client, userdata, flags, rc):
+# 	print("Connected to test.mosqsuitto.org")
+# 	print("Result Code: " + str(rc))
+
+# if __name__ == '__main__':
+# 	#create a client object
+# 	client = mqtt.Client()
+	
+# 	#attach the on_connect() callback function defined above to the mqtt client
+# 	client.on_connect = on_connect
+
+# 	client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+
+# 	client.loop_start()
+# 	time.sleep(1)
+# 	count = 0
+# 	while True:
+# 		byteArr = 0
+# 		with open("./rpicamexample.jpg",'rb') as file:
+# 			print("Reading Image File")
+# 			filecontent = file.read()
+# 			byteArr = bytearray(filecontent)
+	
+
+# 		print("Publishing...")
+# 		client.publish("piwatch/send_img", byteArr)
+
+# 		time.sleep(2)
+# 		count += 1
+# 		if(count == 5):
+# 			break
