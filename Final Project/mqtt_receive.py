17f713fbfbb39f1ce37c774be04621c804ce44b3
alex-frederic <arfreder@usc.edu>
2025-12-01 12:11:39 -0800

    Renamed mqtt file and implemented basic sub code

---
+++ Final Project/mqtt_receive.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected to test.mosquitto.org")
+	print("Result Code: " + rc)
+
+	client.subscribe("arfreder/send_img")
+	client.message_callback_add("arfreder/send_img", on_receive_img)
+	
+def on_receive_img(client, userdata, img):
+	print("Received Image!")
+	print(img)
+
+if __name__ == '__main__':
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+	client.loop_forever()
