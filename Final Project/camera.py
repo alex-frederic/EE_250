f4c0fd2ebde0c29157209a05ef2812af4ac5082b
alex-frederic <arfreder@usc.edu>
2025-12-02 18:40:23 -0800

    blocked in basic camera.py functionality

--- Final Project/camera.py
+++ Final Project/camera.py
@@ -1,8 +1,16 @@
 import picamera
 from time import sleep
+# from yolo import runInference
 
 camera = picamera.PiCamera()
+
+
 camera.start_preview()
+
 sleep(5)
-camera.capture('./test_img.jpg')
+camera.capture('./scan.jpg')
+
+# with open("./scan.jpg") as file:
+
+
 camera.stop_preview()
