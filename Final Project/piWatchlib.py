270c22887bca28c5a60427faa54a50fc6037e8b2
Peyton Crawford <pcrawfor@usc.edu>
2025-12-03 14:11:22 -0800

    Added piWatchlib

---
+++ Final Project/piWatchlib.py
@@ -0,0 +1,21 @@
+def display_alert():
+    print("ALERT: Human detected!")
+def display_image(image_path):
+    print(f"Replacing previous image with new image @{image_path}")
+def log_image(image_path):
+    print(f"Logging image @{image_path}")
+def current_time():
+    import time
+    return time.strftime("%Y%m%d-%H%M%S")
+
+def send_image(image_path='image.jpg', HOST='172.20.10.5', PORT=65432):
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        
+        # Read the image file in binary mode
+        with open(image_path, 'rb') as f:
+            image_data = f.read()
+        
+        s.sendall(image_data)
+        print("Image sent successfully.")
