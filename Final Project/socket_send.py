a776ecd2ac0a5669b066d53a7eff2512b3cc562c
Peyton Crawford <pcrawfor@usc.edu>
2025-12-02 19:11:13 -0800

    Got YOLO working and sending files via TCP

--- Final Project/socket_send.py
+++ Final Project/socket_send.py
@@ -1,4 +1,6 @@
 import socket
+from yolo import runInference
+import cv2
 
 HOST = '172.20.10.3'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
@@ -7,9 +9,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     
     # Read the image file in binary mode
-    with open('test.jpg', 'rb') as f:
-        image_data = f.read()
-    
+    # with open('image.jpg', 'rb') as f:
+    #     image_data = f.read()
+    annotated = runInference('image.jpg')
     # Send the image data
-    s.sendall(image_data)
+    success, encoded = cv2.imencode(".jpg", annotated)
+    img_bytes = encoded.tobytes()
+    s.sendall(img_bytes)
     print("Image sent successfully.")
--- Final Project/yolo.py
+++ Final Project/yolo.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 import cv2
 
-def runInference(img):
+def runInference(img_path):
     # Load model (choose yolov8n.pt for smallest model)
     model = YOLO("yolov8n.pt")
 
     # Load Image and Run inference
-    results = model(cv2.imread(img))
+    results = model(cv2.imread(img_path))
 
     # Return annotated result
     annotated = results[0].plot()
