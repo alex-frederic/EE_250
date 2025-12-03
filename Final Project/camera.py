a49b00209cf336d6927b47f2af63dee97395b82d
alex-frederic <arfreder@usc.edu>
2025-12-03 13:58:13 -0800

    Functionalized campera.py

--- Final Project/camera.py
+++ Final Project/camera.py
@@ -1,16 +1,11 @@
 import picamera
 from time import sleep
-# from yolo import runInference
 
-camera = picamera.PiCamera()
+def take_pic():
+	camera = picamera.PiCamera()
+	camera.start_preview()
 
+	sleep(5)
+	camera.capture('./piwatch_photo.jpg')
 
-camera.start_preview()
-
-sleep(5)
-camera.capture('./scan.jpg')
-
-# with open("./scan.jpg") as file:
-
-
-camera.stop_preview()
+	camera.stop_preview()
