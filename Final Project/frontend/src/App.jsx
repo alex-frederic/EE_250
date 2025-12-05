da2f79d38eea3b2810c5c9d2a85c7d8a418b7e20
alex-frederic <arfreder@usc.edu>
2025-12-04 16:13:35 -0800

    Basic image and log sketched in

--- Final Project/frontend/src/App.jsx
+++ Final Project/frontend/src/App.jsx
@@ -6,7 +6,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      
+      <div id="header">
+        <h1>PiWatch</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div id="curr-img" className="col-8">
+            <img src="img/image.jpg" alt="Currently Displayed Image" />
+          </div>
+          <div id="log" className="col-4">
+            <h2>Log</h2>
+
+            {/* PUT LOG ENTRIES HERE */}
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
