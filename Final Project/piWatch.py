a21562a17e303a55375e884fff5cc46f9bedfb5f
alex-frederic <arfreder@usc.edu>
2025-12-08 19:05:27 -0800

    Added 5 second sleep to piWatch while loop

--- Final Project/piWatch.py
+++ Final Project/piWatch.py
@@ -28,6 +28,8 @@ def main(HOST='127.0.0.1', PORT=65432, image_path='piwatch_photo.jpg'):
         print("Image sent successfully.")
         # send_image('image.jpg', '127.0.0.1', 65432)
         s.close()
+
+        time.sleep(5)
     
 
 if __name__ == "__main__":
