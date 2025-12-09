9a075933e8f7ae20a84e022bdc6d9ff4c792b373
alex-frederic <arfreder@usc.edu>
2025-12-08 22:23:01 -0800

    Reverted app.jsx to Alex's implementation wihout currImg

--- Final Project/frontend/src/App.jsx
+++ Final Project/frontend/src/App.jsx
@@ -7,40 +7,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
-  const [currImg, setCurrImg] = useState("img/placeholder.jpg");
   const [logImgs, setLogImgs] = useState([]);
   const [showLogImg, setShowLogImg] = useState("");
 
   let logEntryKey = 1;
 
   async function pollingFunc(){
-    try {
-      const currResponse = await fetch("http://127.0.0.1:5000/curr_img");
-      const currResults = await currResponse.json();
-      setCurrImg(currResults.curr_img);
-    } catch (error) {
-      console.error("Error fetching current image:", error);
-    }
-
-    try {
-      const logResponse = await fetch("http://127.0.0.1:5000/log_img");
-      const logResults = await logResponse.json();
-      console.log(logResults);
-      setLogImgs(logResults.log);
-    } catch (error) {
-      console.error("Error fetching log images:", error);
-    }
+    const logResponse = await fetch("http://127.0.0.1:5000/log_img");
+    const logResults = await logResponse.json();
+    console.log(logResults);
+    setLogImgs(logResults.log);
   }
 
   useEffect( () => {
-    // Initial fetch
     pollingFunc();
 
-    const intervalId = setInterval( () => {
-      pollingFunc();
-    }, 5000); // Poll every 5 seconds
+    function startPolling () {
+      setInterval( () => {
+        pollingFunc();
+      }, 1000);
+    }
 
-    return () => clearInterval(intervalId);
+    startPolling();
   },
   []);
 
@@ -55,17 +43,12 @@ function App() {
       </div>
       <div className="container">
         <div className="row">
-          <div id="curr-img" className="col-8">
-            <h2>Current Live Image</h2>
-            <img src={currImg} alt="Currently Displayed Image" style={{ maxWidth: '100%', marginBottom: '20px' }} />
-            
+          <div id="curr-img" className="col-8">            
             <h2>Selected Log Image</h2>
             <img src={showLogImg} alt={showLogImg || "Select an image from log"} style={{ maxWidth: '100%' }} />
           </div>
           <div id="log" className="col-4">
             <h2>Log</h2>
-
-            {/* PUT LOG ENTRIES HERE */}
 
             {logImgs.map( (entry) => {
               logEntryKey ++;
